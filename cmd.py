204084d6d15e45819c27c53d87cd6bda5fd26a64
toure <tdunnon@gmail.com>
2015-08-20 16:53:29 -0400

    factory created.

--- cmd.py
+++ cmd.py
@@ -4,46 +4,12 @@ __version__ = "0.1"
 __email__ = "toure@redhat.com"
 __status__ = "Alpha"
 
-from cmd2 import Cmd
-from libs.openstack import keystone
+import code
+from libs.openstack import basestack
 
-class Rhea(Cmd):
-    """Command processor which will allow users to test Features of Openstack Deployment
-    from a command line shell, it will also have the ability to save config as an output for
-    test plans."""
-    prompt = 'Rhea > '
-    intro = "Command line environment which enables functional testing."
 
-    def do_setup(self, line):
-        """
-        Setup will be responsible for configuring services and openstack
-        instances.
-        :param line:
-        :return:
-        """
-        print "hello"
-
-    def do_deploy(self, line):
-        """
-        Deploy will install an openstack instances based on saved config.
-        :param line:
-        :return:
-        """
-        return True
-
-    def do_config(self, arg):
-        """
-        Configu will switch name space to allow for the creation of a .
-        :param arg:
-        :return:
-        """
+class Iridium(code.InteractiveConsole, basestack):
+    def __init__(self):
         pass
 
-    def do_auth(self, args):
-        """
-        Auth will be responsible for creating the keystone object from the command line.
-        :param args:
-        :return: keystone session for the command line.
-        """
-        keystone.create_keystone(args)
-
+    def shell(self, component):
--- libs/openstack/basestack.py
+++ libs/openstack/basestack.py
@@ -6,16 +6,34 @@ __status__ = "Alpha"
 
 
 class BaseStack(object):
-    def __init__(self, component, **kwargs):
-        """
-
-        :type kwargs: authentication dictionary for keystone.
-        """
+    """
+    Base stack class is responsible for returning a list of methods from a specified module name.
+    """
+    def __init__(self, component):
         self.component = component
-        self.auth_dict = kwargs
 
+    def import_mod(self, module_name):
+        """
+        Import mod will return an initialized import path from the specified module name.
 
-    def factory(self):
-        if self.auth_dict:
+        :param module_name: openstack module name of interest (str).
+        :return: import object of requested module name.
+        """
+        return __import__("libs.openstack.%s" % module_name)
 
+    def factory(self, component):
+        """
+        Factory is responsible for returning a list of methods from the component name space, but
+        instead of returning everything it filters out the private and built-in callables.
 
+        :rtype : list of methods from component name space.
+        """
+        methodlist = [method for method in dir(component)
+                      if callable(getattr(component, method) and not method.startswith('__'))]
+        return methodlist
+
+    def component_path(self):
+        """
+        Component path is responsible for returning the output from the closure function, which returns a method list.
+        """
+        return self.factory(self.import_mod(self.component))
