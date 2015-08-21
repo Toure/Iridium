ce8dd3e96e83da3f224552b812016ac54946bab9
toure <tdunnon@gmail.com>
2015-08-20 23:41:47 -0400

    working on custom project repl.

--- cmd.py
+++ cmd.py
@@ -4,12 +4,27 @@ __version__ = "0.1"
 __email__ = "toure@redhat.com"
 __status__ = "Alpha"
 
-import code
+from code import InteractiveConsole
 from libs.openstack import basestack
 
 
-class Iridium(code.InteractiveConsole, basestack):
-    def __init__(self):
-        pass
+class Iridium(InteractiveConsole):
+    def interact(self):
+        banner = "=====" * 3 + "\n" + "Welcome to Iridium" + "\n" + "VERSION: 0.1" + "\n" + "=====" * 3
+        super(Iridium, self).interact(banner)
 
-    def shell(self, component):
+    def raw_input(self, prompt=None):
+        prompt = "iridium >"
+        return raw_input
+
+
+class LoadFunc(Iridium, basestack):
+
+    def findfunc(self):
+        pass
+        # TODO add logic to scan for modules from basestack.
+
+    def createlocale(self):
+        pass
+        console = Iridium(locals=function_list)
+        return console.interact()
