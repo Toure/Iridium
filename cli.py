b84a335508b1c66668147b2eaa8b5911c7e026b6
tdunnon@gmail.com <100yard->
2015-08-21 12:27:59 -0400

    created iridium's shell.

---
+++ cli.py
@@ -0,0 +1,34 @@
+__author__ = "Toure Dunnon"
+__license__ = "Apache License 2.0"
+__version__ = "0.1"
+__email__ = "toure@redhat.com"
+__status__ = "Alpha"
+
+
+from code import InteractiveConsole
+from libs.openstack import basestack
+
+
+class Iridium(InteractiveConsole, object):
+
+    def interact(self, banner=None):
+        banner = "Welcome to Iridium"
+        super(Iridium, self).interact(banner)
+
+    def raw_input(self, prompt=""):
+        prompt = "iridium > "
+        return raw_input(prompt)
+
+local_modules = locals()
+stack_modules = basestack.Basestack()
+local_modules['nova'] = stack_modules.import_mod('nova')
+local_modules['keystone'] = stack_modules.import_mod('keystone')
+local_modules['ironic'] = stack_modules.import_mod('ironic')
+local_modules['heat'] = stack_modules.import_mod('heat')
+local_modules['swift'] = stack_modules.import_mod('swift')
+local_modules['glance'] = stack_modules.import_mod('glance')
+local_modules['cinder'] = stack_modules.import_mod('cinder')
+local_modules['neutron'] = stack_modules.import_mod('neutron')
+
+console = Iridium(locals=local_modules)
+console.interact()
--- libs/openstack/basestack.py
+++ libs/openstack/basestack.py
@@ -4,36 +4,17 @@ __version__ = "0.1"
 __email__ = "toure@redhat.com"
 __status__ = "Alpha"
 
+from importlib import import_module
 
-class BaseStack(object):
-    """
-    Base stack class is responsible for returning a list of methods from a specified module name.
-    """
-    def __init__(self, component):
-        self.component = component
 
-    def import_mod(self, module_name):
+class Basestack(object):
+
+    @staticmethod
+    def import_mod(module_name):
         """
         Import mod will return an initialized import path from the specified module name.
 
         :param module_name: openstack module name of interest (str).
         :return: import object of requested module name.
         """
-        return __import__("libs.openstack.%s" % module_name)
-
-    def factory(self, component):
-        """
-        Factory is responsible for returning a list of methods from the component name space, but
-        instead of returning everything it filters out the private and built-in callables.
-
-        :rtype : list of methods from component name space.
-        """
-        methodlist = [method for method in dir(component)
-                      if callable(getattr(component, method)) and not method.startswith('__')]
-        return methodlist
-
-    def component_path(self):
-        """
-        Component path is responsible for returning the output from the closure function, which returns a method list.
-        """
-        return self.factory(self.import_mod(self.component))
+        return import_module("libs.openstack.%s" % module_name)
