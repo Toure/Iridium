e6cdcd7a3b065359de5e1ce2ac213135ab103387
tdunnon@gmail.com <100yard->
2015-09-10 22:00:39 -0400

    clean up.

--- iridium/tests/__init__.py
+++ iridium/tests/__init__.py
@@ -4,6 +4,6 @@ __version__ = "0.1"
 __email__ = "toure@redhat.com"
 __status__ = "Alpha"
 """
-This will be the standard location for all test which are separated via component name, which will allow
+This will be the standard location for all test that are separated via component name, which will allow
 for flexible test execution, instead of a all or nothing approach.
 """
