52c1085bac8bff79db6da8f2f907bd929cecbfc2
Toure <toure@redhat.com>
2016-04-26 15:38:15 -0400

    basic structure for task manager.

---
+++ iridium/core/tasker.py
@@ -0,0 +1,20 @@
+"""
+Tasker will take a command from that call it will inspect the registered list of functions / methods and
+place it into a asynchronous task, this will allow users to spawn multiple functions and not block the terminal.
+"""
+
+import asyncio
+
+
+class Tasker(object):
+    def __init__(self, cmd, *args):
+        pass
+
+    def lookup(self):
+        pass
+
+    def launcher(self):
+        pass
+
+    def status(self):
+        pass
