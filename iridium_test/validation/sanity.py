64e7aadcf30721eb4c76be9c3bfb1cd59818b98f
Toure <toure@redhat.com>
2016-06-30 07:47:39 -0400

    validation function to be added.

---
+++ iridium_test/validation/sanity.py
@@ -0,0 +1,14 @@
+import pytest
+from iridium_test import nova_tests
+from iridium_test import nova_tests
+from iridium_test import nova_tests
+from iridium_test import nova_tests
+
+
+
+def test_sanity():
+    """
+    Sanity test will run through the basic test and output the result in a
+    xunit file with the help of py.test.
+    :return: xunit test result.
+    """
